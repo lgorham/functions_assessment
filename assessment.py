763e6408f3bb5b3a8e2fb845b0af78915d13547f
Lydia Gorham <gorhamlydia@gmail.com>
2016-07-10 19:35:34 -0700

    Completed Part 3

--- assessment.py
+++ assessment.py
@@ -123,10 +123,26 @@ def hometown_greeting(town_name, first_name, last_name):
 #    inside of it. The outer function should take ``x``, an integer which
 #    defaults to 1. The inner function should take ``y`` and add ``x`` and ``y`` together.
 
+def increment(x=1):
+    def add(y):
+        return x + y
+    return add
+
+
+
 # 2. Call the function ``increment()`` with x = 5. Assign what is returned to a variable name, addfive. Call 
 #    addfive with y = 5. Call again with y = 20.
 
+addfive = increment(5)
+addfive(5)
+addfive(20)
+
 # 3. Make a function that takes in a number and a list of numbers. It should append
 #    the number to the list of numbers and return the list.
+
+def append_num(list, x):
+    list.append(x)
+    return list
+
 
 #####################################################################
